842374102f32622be6158d2ab47f2e41c41106b6
usvardaanisdabest <78273678+usvardaanisdabest@users.noreply.github.com>
2021-09-27 00:01:39 +0530

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,5 +1,5 @@
 #wget https://raw.githubusercontent.com/usvardaanisdabest/nodejs-test/main/2.sh && chmod +x 2.sh
 #nohup ./2.sh &
 #nohup python3 -m http.server 8080 &
-service apache2 start && service mysql start
+sudo /sbin/service apache2 restart && sudo /sbin/service mysql restart
 socat tcp-listen:$PORT,reuseaddr,fork tcp:localhost:80
